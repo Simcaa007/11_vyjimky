8f82d03202c04a0f48fce0fbf9028344eaac60d1
Simcaa007 <havelkova.si@seznam.cz>
2026-04-06 15:20:29 +0200

    nejde

---
+++ 08/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int c = 0;
+            int[] pole = new int[textBox1.Lines.Length]; int i = 0;
+            foreach (string s in textBox1.Lines)
+            {
+                try
+                {
+                    if (int.TryParse(s, out c))
+                    {
+                        pole[i] = c;
+                        i++;
+                    }
+                    else
+                    {
+                        throw new FormatException();
+                    }
+                    if (c > int.MaxValue || c < int.MinValue)
+                    {
+                        throw new OverflowException();
+                    }
+                }
+                catch (FormatException)
+                {
+                    pole[i] = 0; i++;
+                }
+                catch (OverflowException)
+                {
+                    if (c > int.MaxValue)
+                    {
+                        pole[i] = int.MaxValue;
+                    }
+                    else if (c < int.MinValue)
+                    {
+                        pole[i] = int.MinValue;
+                    }
+                }
+            }
+
+            foreach (int p in pole)
+            {
+                listBox1.Items.Add(p);
+            }
+        }
+    }
+}
---
+++ 08/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("08")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("08")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("fdf6e533-6c5b-440c-ab3e-b4d9b3df0243")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
