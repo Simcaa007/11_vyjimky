2592bcfa05c94bc99a9dee96cc43c95a61d4bb1e
Simcaa007 <havelkova.si@seznam.cz>
2026-04-06 10:40:25 +0200

    hotovo

--- 01/Form1.cs
+++ 01/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace _01
 {
@@ -15,6 +16,41 @@ namespace _01
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int soucin = 1;
+            int cislo = 0;
+            try
+            {
+                using (StreamReader sr = new StreamReader("..\\..\\cisla.txt"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        try
+                        {
+                            if (int.TryParse(sr.ReadLine(), out cislo))
+                            {
+                                checked
+                                {
+                                    soucin *= cislo;
+                                }
+                            }
+                        }
+                        catch (OverflowException)
+                        {
+                            MessageBox.Show("Došlo k přetečení!!", "Problem bejby...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+
+                MessageBox.Show($"soucin je: {soucin}");
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("Soubor nebyl naleznut!", "Problem bejby...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
