5bea5b6863dcc8fbeb63b89689d6974b424b90f6
Simcaa007 <havelkova.si@seznam.cz>
2026-04-06 11:31:42 +0200

    hotovo

---
+++ 02/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = 0;
+                if (int.TryParse(textBox1.Text, out n))
+                {
+                    Random rnd = new Random();
+                    int[] pole = new int[n];
+                    for (int i = 0; i < n; i++)
+                    {
+                        pole[i] = rnd.Next(1, 21);
+                        listBox1.Items.Add(pole[i]);
+                    }
+                    double prvni = pole[0];
+                    double sesty = pole[5];
+                    try
+                    {
+                        int mocnina;
+                        checked
+                        {
+                            mocnina = (int)Math.Pow(prvni, sesty);
+                        }
+                        MessageBox.Show($"mocnina {prvni} na {sesty} je {mocnina}");
+                    }
+                    catch (OverflowException)
+                    {
+                        MessageBox.Show("Doslo k preteceni");
+                    }
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                MessageBox.Show("Doslo k preteceni pri cteni do pole.");
+            }
+        }
+    }
+}
