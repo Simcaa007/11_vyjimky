ff1046f81eda91e540200358afb52babced6bac0
Simcaa007 <havelkova.si@seznam.cz>
2026-04-06 15:36:12 +0200

    hotovo

---
+++ 09/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int c1 = 0; int c2 = 0;
+            try
+            {
+                if (int.TryParse(textBox1.Text, out c1))
+                {
+                }
+                else
+                {
+                    throw new FormatException("prvni textbox neobsahuje cislo");
+                }
+            }
+            catch(FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+                textBox1.Focus();
+            }
+
+            try
+            {
+                if (int.TryParse(textBox2.Text, out c2))
+                {
+                }
+                else
+                {
+                    throw new FormatException("druhy textbox neobsahuje cislo");
+                }
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+                textBox2.Focus();
+            }
+
+            try
+            {
+                int soucin = c1 * c2;
+                MessageBox.Show($"soucin je {soucin}");
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("pri nasobeni doslo k preteceni");
+            }
+            try
+            {
+                double deleni = c1 / c2;
+                MessageBox.Show($"deleno je {deleni}");
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("vazne chces delit nulou jo?");
+            }
+        }
+    }
+}
