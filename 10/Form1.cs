8926522898c756f2f63fd37300292e90988179e9
Simcaa007 <havelkovas.07@spst.eu>
2026-04-07 09:16:17 +0200

    hotovo

--- 10/Form1.cs
+++ 10/Form1.cs
@@ -71,18 +71,45 @@ namespace _10
                 textBox1.Focus();
             }
 
-            if (b != 0)
+            try
             {
-                double d = (b * b) - (4 * a * c);
-
-                if (d > 0)
+                if (a != 0 && a != null)
                 {
-                    double x1 = (-a * Math.Sqrt(d))
+                    throw new Exception("kvadraticka");
+                }
+                else
+                {
+                    throw new Exception("linearni");
                 }
             }
-            else
+            catch (Exception ex)
             {
+                if (ex.Message == "kvadraticka")
+                {
+                    double x1 = 0; double x2 = 0;
+                    double d = (b * b) - (4 * a * c);
 
+                    if (d > 0)
+                    {
+                        x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                        x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                    }
+
+                    MessageBox.Show($"koreny rovnice jsou: {x1} a {x2}");
+                }
+                else
+                {
+                    try
+                    {
+                        double vys = c / b;
+
+                        MessageBox.Show($"vysledek linearni r-ce je {vys}");
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        MessageBox.Show("Rovnice nema reseni.");
+                    }
+                }
             }
         }
     }
